48b4faecf7865c8e25fcd753280991efd551486d
lurr98 <laurazeidler.konstanz@gmail.com>
2025-05-21 14:52:15 +0200

    changed instruction format

--- implicit_meaning_task/common/utils.py
+++ implicit_meaning_task/common/utils.py
@@ -5,7 +5,8 @@ from core.scripts.utils import display_progress, load_annotation, TASK_INFO
 def remove_punctuation(text: str) -> str:
 
     text = " ".join(text.split("\n"))
-    return re.sub(r"[”#*\+/<=>\[\]\\^_`{|}~]", "", text)
+    subbed_text = re.sub(r"\d\.", "", text)
+    return re.sub(r"[”#*\+/<=>\[\]\\^_`{|}~]", "", subbed_text)
 
 
 def format_sample(question: dict) -> None:
