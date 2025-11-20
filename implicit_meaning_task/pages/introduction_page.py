900707953f72eba013363351ee6a12e735ab393b
lurr98 <laurazeidler.konstanz@gmail.com>
2025-11-20 17:00:16 +0100

    updated annotation guidelines

--- implicit_meaning_task/pages/introduction_page.py
+++ implicit_meaning_task/pages/introduction_page.py
@@ -8,27 +8,33 @@ first_md, second_md = split_md[0], split_md[1]
 
 st.write(first_md)
 implicit = st.segmented_control("", ["Yes", "No"])
-
+ 
 if implicit == "No":
     st.markdown("Please specify one or multiple reasons for your choice:")
-
     col1, col2 = st.columns(2)
-
     with col1:
-        context = st.checkbox(label="Context")
-        reasoning = st.checkbox(label="Logical Reasoning")
-        complement = st.checkbox(label="Expected Information")
-        instruction = st.checkbox(label="Recoverable Instruction")
-
+        context = st.checkbox(label="Context", help="The added information is recoverable from the context.")
+        reasoning = st.checkbox(label="Logical Reasoning", help="The added information is a logical premise or consequence given some mutual knowledge that the author can expect from the reader.")
+        background = st.checkbox(label="Background Knowledge", help="The information in the added text was already anticipated due to existing background knowledge.")
     with col2:
         other = st.checkbox("Other")
-        comment_implicit = st.text_input(label="Specify any other reason that led to your decision:")
+        comment_implicit = st.text_input(label="If applicable, specify other reasons for your decision:", max_chars=200)
         if comment_implicit:
             st.write(r"$\textsf{\scriptsize Thanks for your input!}$")
-else:
-    comment_not_implicit = st.text_input(label="If you are unsure, select \"Yes\" and explain your thoughts here:")
-    if comment_not_implicit:
-        st.write(r"$\textsf{\scriptsize Thanks for your input!}$")
+checkboxes = [context, reasoning, background, other]
+
+st.write("")
+st.write("")
+confidence = st.radio(
+"How confident are you about your annotation?\n\n1 corresponds to 'Not at all' and 5 to 'Very much'.",
+["1", "2", "3", "4", "5"],
+index=None,
+horizontal=True
+)
+st.write("")
+comment_not_implicit = st.text_input(label="Anything you'd like to point out?", max_chars=200)
+if comment_not_implicit:
+    st.write(r"$\textsf{\scriptsize Thanks for your input!}$")
 
 
 st.write(second_md)
