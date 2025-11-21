ab881b2fe3bc1dd2aaef1a89ad67a695e3b55a65
lurr98 <laurazeidler.konstanz@gmail.com>
2025-11-21 12:19:09 +0100

    modified context display

--- implicit_meaning_task/common/utils.py
+++ implicit_meaning_task/common/utils.py
@@ -5,7 +5,7 @@ from core.scripts.utils import display_progress, read_json_from_file, load_annot
 def remove_punctuation(text: str) -> str:
 
     # remove timestamps
-    text = re.sub(r"Timestamp.*Z", "", text)
+    text = re.sub(r".*Timestamp.*Z", "", text)
     text = "\n".join([el for el in text.split("\n") if el])
     text = re.sub(r"\s+(?=\d)", " ", text)
     # remove listed numbers before a line break
